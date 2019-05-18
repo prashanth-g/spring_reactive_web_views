a414ec2963bb640a93e37577ba3ada2234afd82f
prashanth-g <prashanthcsgce@gmail.com>
2019-05-18 21:15:28 +0530

    Added boot class

---
+++ src/main/java/com/prashanth/spring/reactive/web/ReactiveViewsApplication.java
@@ -0,0 +1,11 @@
+package com.prashanth.spring.reactive.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveViewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveViewsApplication.class, args);
+    }
+}
