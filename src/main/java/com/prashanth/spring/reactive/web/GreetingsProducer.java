e446c02a7efa99c092f182ad3e5bbf3f4939c58c
prashanth-g <prashanthcsgce@gmail.com>
2019-05-24 18:21:49 +0530

    Added websocket handler

--- src/main/java/com/prashanth/spring/reactive/web/GreetingsProducer.java
+++ src/main/java/com/prashanth/spring/reactive/web/GreetingsProducer.java
@@ -1,8 +1,6 @@
 package com.prashanth.spring.reactive.web;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.prashanth.spring.reactive.web.model.Greeting;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -13,16 +11,10 @@ import java.util.stream.Stream;
 
 @Component
 public class GreetingsProducer {
-    Flux<Greeting> greet(String name) {
+    public Flux<Greeting> greet(String name) {
         return Flux
                 .fromStream(Stream.generate(() -> new Greeting("Hello "+name+ " @ " + Instant.now() + "!")))
                 .delayElements(Duration.ofSeconds(1));
     }
 }
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Greeting {
-    private String message;
-}
--- src/main/java/com/prashanth/spring/reactive/web/websocket/WebsocketConfiguration.java
+++ src/main/java/com/prashanth/spring/reactive/web/websocket/WebsocketConfiguration.java
@@ -1,9 +1,16 @@
 package com.prashanth.spring.reactive.web.websocket;
 
+import com.prashanth.spring.reactive.web.GreetingsProducer;
+import com.prashanth.spring.reactive.web.model.Greeting;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +26,26 @@ public class WebsocketConfiguration {
                 Map<String, Object> urlMap = new HashMap<>();
                 urlMap.put("/ws/greetings", webSocketHandler);
                 setUrlMap(urlMap);
+            }
+        };
+    }
+
+    @Bean
+    WebSocketHandlerAdapter webSocketHandlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+
+    @Bean
+    WebSocketHandler webSocketHandler(GreetingsProducer greetingsProducer) {
+        return new WebSocketHandler() {
+            @Override
+            public Mono<Void> handle(WebSocketSession session) {
+                Flux<WebSocketMessage> message = session.receive()
+                        .map(WebSocketMessage::getPayloadAsText)
+                        .flatMap(greetingsProducer::greet)
+                        .map(Greeting::getMessage)
+                        .map(session::textMessage);
+                return session.send(message);
             }
         };
     }
