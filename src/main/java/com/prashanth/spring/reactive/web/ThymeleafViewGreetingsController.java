5247f87139cfad021a933500b58eb66702262f54
prashanth-g <prashanthcsgce@gmail.com>
2019-05-27 19:56:28 +0530

    Updated thymeleaf view controller

--- src/main/java/com/prashanth/spring/reactive/web/ThymeleafViewGreetingsController.java
+++ src/main/java/com/prashanth/spring/reactive/web/ThymeleafViewGreetingsController.java
@@ -1,14 +1,28 @@
 package com.prashanth.spring.reactive.web;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 
 @Controller
+@AllArgsConstructor
 public class ThymeleafViewGreetingsController {
+
+    private final GreetingsProducer greetingsProducer;
 
     @GetMapping("/greetings.do")
     String renderView() {
         return "greetings";
+    }
+
+    @GetMapping(value = "/greetings-view.do", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    String renderUpdatedView(@RequestParam("name") String name, Model model) {
+        model.addAttribute("greetings", new ReactiveDataDriverContextVariable(this.greetingsProducer.greet(name), 1));
+        return "greetings :: #greetings-block";
     }
 
 }
